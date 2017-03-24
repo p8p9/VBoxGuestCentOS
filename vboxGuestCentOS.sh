0eae95715c90aed57d5a7cd02e6c09eae0de10f5
Abel da Silva <abel_p8p9@yahoo.com.br>
2017-03-23 21:57:40 -0300

    Update vboxGuestCentOS.sh
    adição de comentários ao script

--- vboxGuestCentOS.sh
+++ vboxGuestCentOS.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#Ativação do repositório EPEL no CentOS
 yum --enablerepo=extras install epel-release
+#Atualização dos repositórios e do sistema
 yum update -y
 yum upgrade -y
 yum install -y make dkms gcc kernel-devel-$(uname -r)
